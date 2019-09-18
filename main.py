a54cc4fce52549ee08396998de2aa4d6abb81fdc
Nathalia <natylafosse@gmail.com>
2019-09-17 20:28:46 -0400

    basic form and welcome page

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, request, redirect, render_template
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+@app.route("/add", methods=['POST'])
+def add_info():
+    username = request.form['username']
+    password = request.form['password']
+
+    return render_template('welcome.html', username=username)
+
 
 @app.route("/")
 def index():
-    return form.format('')
+    return render_template('index.html')
 
 app.run()
