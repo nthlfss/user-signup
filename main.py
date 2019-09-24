4d1d3fd38aa69c88c6c46ea8b5d95a887d214e90
Nathalia <natylafosse@gmail.com>
2019-09-24 19:42:29 -0400

    created error msg for username

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-@app.route("/add", methods=['POST'])
-def add_info():
-    username = request.form['username']
-    password = request.form['password']
-
-    return render_template('welcome.html', username=username)
-
-
+# load home page
 @app.route("/")
 def index():
     return render_template('index.html')
+
+
+# load welcome page with username
+@app.route("/welcome", methods=['GET','POST'])
+def welcome():
+    username = request.form['username']
+    usernameError = ""
+
+    if " " in username:
+        usernameError = "That's not a valid username"
+        return render_template ("index.html", username=username, usernameError=usernameError)
+    else:
+        return render_template("welcome.html", username=username)
 
 app.run()
